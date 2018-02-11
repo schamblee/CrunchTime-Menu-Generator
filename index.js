0329983f44435afaeeb4d0978ab46b1b7849f773
Stephanie Chamblee <stephaniejoychamblee@gmail.com>
2018-02-11 07:06:12 -0500

    added diet filter, calorie and protein count

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
 const EDAMAM_SEARCH_URL = 'https://api.edamam.com/search';
 
-function getDataFromApi(searchTerm, callback) {
+
+function getDataFromApi(searchTerm, filter, callback) {
   const query = {
     q: `${searchTerm}`,
-    app_id: '15ee63e4',
+    diet: `${filter}`,
     from: 0,
-    to: 9,
+    to: 12,
+    app_id: '15ee63e4',
     app_key: 'd955b5e8d8941a9bf3c4736e3b6b08e7'
   }
   $.getJSON(EDAMAM_SEARCH_URL, query, callback);
 }
 
 function renderResult(result) {
+  let calories = `${result.recipe.calories}`
+
+  let protein = `${result.recipe.totalNutrients.PROCNT.quantity}`
+  let servings = `${result.recipe.yield}`
+  let calorieCount = (calories/servings).toFixed();
+  let proteinCount = (protein/servings).toFixed();
+
   return `<div class="col-4"> 
     <div class="recipe-card">
       <div class="recipe-title">
         <h3>${result.recipe.label}</h3>
-        <p>
+        <p>Calories: ${calorieCount}</p>
+        <p>Protein: ${proteinCount}</p>
       </div>
       <a class="js-result-name card-image" href="${result.recipe.url}" target="_blank"><img src="${result.recipe.image}" alt="${result.recipe.label}"></a>
     </div>
@@ -37,9 +47,13 @@ function watchSubmit() {
     $('.js-output').prop('hidden', false);
     const queryTarget = $(event.currentTarget).find('.js-query');
     const query = queryTarget.val();
+    const filterTarget = $(event.currentTarget).find('.diet-filter')
+    const dietFilter = filterTarget.val();
     // clear out the input
     queryTarget.val("");
-    getDataFromApi(query, displayRecipeData);
+    getDataFromApi(query, dietFilter, displayRecipeData);
+    let carbs = `${result.recipe.totalNutrients.CHOCDF.quantity}`
+    console.log(carbs);
   });
 }
 

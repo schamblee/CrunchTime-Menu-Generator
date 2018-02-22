67eea33181be5c729107f2537701d764f4bcc217
Stephanie Chamblee <stephaniejoychamblee@gmail.com>
2018-02-21 20:27:55 -0500

    start over and new menu buttons, remove recipe btn fix

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 
-let dietFilter = '';
-let dayIndex = -1;
+//Global variables are set by the user to indicate their diet and allergy information
 let allergies = [];
+let dietFilter = '';
+
+
+let dayIndex = -1;
 let offset = 1
 
 function getRecipesForWeek(allergies, diet) { 
@@ -18,23 +21,16 @@ function getRecipesForWeek(allergies, diet) {
 
 
 function getRecipeForDay(allergies, diet, day, query, offset) { 
-  dayCard = `${day}Card`
   $.ajax({
     url: `https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/searchComplex?query=${query}&diet=${diet}&addRecipeInformation=false&number=1&offset=${offset}&instructionsRequired=true&intolerances=${allergies}&limitLicense=false&maxCalories=600&type=main+course`,
       type: 'GET',
       dataType: 'json',
-      success: function (result) { displayRecipeForDay(result, dayCard)
+      success: function (result) { displayRecipeForDay(result, day)
       },
       error: function() { alert('boo!'); },
       beforeSend: setHeader
       });
   };
-
-
-function displayRecipeForDay(data, day) {
-  const results = data.results.map((item, day, index) => renderDayCard(item, day));
-  $(`.${day}`).html(results);
-}
 
 
 
@@ -51,6 +47,11 @@ function getRecipeInfo(id) {
       error: function() { alert('boo!'); },
       beforeSend: setHeader
       });
+}
+
+function displayRecipeForDay(data, day) {
+  const results = data.results.map((item, day, index) => renderDayCard(item, day));
+  $(`#${day}Card`).html(results);
 }
 
 function displayRecipeInfo(data) {
@@ -75,15 +76,22 @@ function renderMenu(offset, result) {
   
   return `
   <div class="col-4"> 
-    <span class="${days[dayIndex]}ingredient-query"></span>
-    <span class = "day-title" value="${days[dayIndex]}">${days[dayIndex]}</span>
-    <div class="recipe-card ${days[dayIndex]}Card">
-        <h3 class="recipe-title">${result.title}</h3>
-        <p>Calories: ${result.calories}</p>
-        <p>Protein: ${result.protein}</p>
-        <a class="js-result-name" href="${result.image}" target="_blank"><img class="card-image" src="${result.image}" alt="${result.title} image"></a>
+    <span id="ingredient-query${days[dayIndex]}" class="ingredient-query"></span>
+    <span id="day-title${days[dayIndex]}" class = "day-title">${days[dayIndex]}</span>
+    <div id="${days[dayIndex]}Card" class="recipe-card">
+        <h3 id="recipe-title${days[dayIndex]}" class="recipe-title">${result.title}</h3>
+        <p id="calories${days[dayIndex]}">Calories: ${result.calories}</p>
+        <p id="protein${days[dayIndex]}">Protein: ${result.protein}</p>
+        <img id="card-image${days[dayIndex]}" class="card-image" src="${result.image}" alt="${result.title} image">
+    </div>
         <!-- Trigger/Open The Modal -->
-        <button id="js-view-recipe-btn" value="${result.id}">View Recipe</button>
+         <button id="js-view-recipe-btn" class="js-view-recipe-btn" value="${result.id}">View Recipe</button>
+
+         <input id="search-by-ingredient${days[dayIndex]}" class="search-by-ingredient" type="search" name="search-by-ingredient" placeholer="Search By Ingredient">
+         <button id="search-by-ingredient-btn" class="search-by-ingredient-btn" value="${days[dayIndex]}">Search</button>
+         <button id="js-next-result-btn${days[dayIndex]}" class="js-next-result-btn" value="${days[dayIndex]}">Next Result</button>
+         <button id="js-previous-result-btn${days[dayIndex]}" class="js-previous-result-btn" value="${days[dayIndex]}" aria-live="assertive" hidden>Previous Result</button>
+         <button id="js-remove-day${days[dayIndex]}" class="js-remove-day" value="${days[dayIndex]}">Remove Day</button>
 
         <!-- The Modal -->
         <div id="recipeModal" class="modal">
@@ -91,21 +99,12 @@ function renderMenu(offset, result) {
         <!-- Modal content -->
       <div class="modal-content">
           <span class="close">&times;</span>
-          <h3 class="recipe-title">${result.title}</h3>
-          <img class="card-image" src="${result.image}" alt="${result.title} image">
-          <section role="region" class="recipe-details">
+          <h3 id="recipe-title${days[dayIndex]}">${result.title}</h3>
+          <img id="card-image${days[dayIndex]}" class="modal-card-image" src="${result.image}" alt="${result.title} image">
+          <section role="region" id="recipe-details${days[dayIndex]}" class="recipe-details">
           </section>
         
-       </div>
-
         </div>
-        <form>
-      <div>
-        <input class="search-by-ingredient" type="search" id="${days[dayIndex]}-search" name="search-by-ingredient" placeholer="Search By Ingredient">
-        <button class="search-by-ingredient-btn" value="${days[dayIndex]}">Search</button>
-      </div>
-        </form>
-        <button class="js-remove-day">Remove Day</button>
       </div>
     </div>
 `
@@ -118,13 +117,8 @@ function renderDayCard(result, day) {
         <h3 class="recipe-title">${result.title}</h3>
         <p>Calories: ${result.calories}</p>
         <p>Protein: ${result.protein}</p>
-      <a class="js-result-name" href="${result.image}" target="_blank"><img class="card-image" src="${result.image}" alt="${result.title}"></a>
-      <button type="button" class="js-view-recipe">View Recipe</button>
-        <input class="search-by-ingredient" type="search" id="${day}-search" name="search-by-ingredient" placeholer="Search By Ingredient">
-        <button class="search-by-ingredient-btn" value="${day}">Search</button>
-        <button class="js-next-result-btn" value="MondayCard">Next Result</button>
-        <button class="js-previous-result-btn" value="MondayCard">Previous Result</button>
-      <button class="js-remove-day">Remove Day</button>
+      <img id="card-image${days}" class="card-image" src="${result.image}" alt="${result.title}">
+
 `
 }
 
@@ -132,13 +126,14 @@ function displayRecipesForWeek(data, recipeIndex) {
   const results = data.results.map((item, recipeIndex, index) => renderMenu(recipeIndex, item));
   $('.js-search-results').html(results);
   // Get the modal
-var modal = document.getElementById('recipeModal');
+let modal = document.getElementById('recipeModal');
+
 
 // Get the button that opens the modal
-var btn = document.getElementById("js-view-recipe-btn");
+let btn = document.getElementById("js-view-recipe-btn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+let span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal 
 btn.onclick = function() {
@@ -162,14 +157,6 @@ window.onclick = function(event) {
 }
 
 
-function watchNextOptionClick() {
-  $('.recipe-card').on('click', '.next-option', function(event) {
-    event.preventDefault()
-    let recipeId = $(event.currentTarget).find('.recipe-title').val()
-    console.log(`the recipe id is ${recipeId}`);
-    })
-}
- 
 
 function watchBeginClick() {
   $('.js-begin-btn').click(event => {
@@ -209,6 +196,8 @@ function watchMenuSubmit() {
     event.preventDefault();
     $('.js-select-days').prop('hidden', true);
     $('.js-output').prop('hidden', false);
+    $('.js-menu-controls').prop('hidden', false);
+    
     dietFilter = ''//filterTarget.val();
     allergyList = ['dairy']
     getRecipesForWeek(allergyList, dietFilter);
@@ -218,46 +207,90 @@ function watchMenuSubmit() {
 function watchSearchByIngredientClick() {
   $('.js-output').on('click', '.search-by-ingredient-btn', function(event) {
   event.preventDefault();
-  let dayCard = $(this).val();
-  let ingredient = $(`#${dayCard}-search`).val();
-  $(`.${dayCard}ingredient-query`).text(`Result for ${ingredient}`)
-  dietFilter = ''//filterTarget.val();
-  allergyList = ['dairy']
+  let day = $(this).val();
+  let ingredient = $(`#search-by-ingredient${day}`).val();
+  $(`#ingredient-query${day}`).text(`Result for ${ingredient}`)
+  dietFilter = ''
+  allergyList = ['']
   offset = Math.floor(Math.random() * 200)
-  getRecipeForDay(allergyList, dietFilter, dayCard, ingredient, offset);
-  console.log(`${dayCard} ${ingredient} option pressed`)
+  getRecipeForDay(allergyList, dietFilter, day, ingredient, offset);
+  console.log(`${day} ${ingredient} option pressed`)
   })
 }
 
 function watchNextResultClick() {
   $('.js-output').on('click', '.js-next-result-btn', function(event) {
   event.preventDefault();
-  let dayCard = 'Monday'
-  let ingredient = 'chicken'
-  dietFilter = ''//filterTarget.val();
+  let day = $(this).val()
+  $(`#js-previous-result-btn${day}`).prop('hidden', false);
+  let ingredient = ''
+  dietFilter = ''
   allergyList = ['dairy']
   offset ++
-  getRecipeForDay(allergyList, dietFilter, dayCard, ingredient, offset);
-  console.log(`${dayCard} ${ingredient} option pressed`)
+  getRecipeForDay(allergyList, dietFilter, day, ingredient, offset);
+  console.log(`${day} ${ingredient} option pressed`)
   })
 }
 
 function watchPreviousResultClick() {
-   $('.js-output').on('click', '.js-previous-result-btn', function(event) {
+  $('.js-output').on('click', '.js-previous-result-btn', function(event) {
   event.preventDefault();
-  let dayCard = 'Monday'
-  let ingredient = 'chicken'
-  $(`.${dayCard}ingredient-query`).text(`Result for ${ingredient}`)
+  let day = $(this).val()
+  let ingredient = ''
   dietFilter = ''//filterTarget.val();
   allergyList = ['dairy']
   offset --
-  getRecipeForDay(allergyList, dietFilter, dayCard, ingredient, offset);
-  console.log(`${dayCard} ${ingredient} option pressed`)
+  getRecipeForDay(allergyList, dietFilter, day, ingredient, offset);
+  console.log(`${day} ${ingredient} option pressed`)
   })
 }
 
+function renderDayCard(result, day) {  
+  return `
+        <h3 id="recipe-title${day}" class="recipe-title">${result.title}</h3>
+        <p id="calories${day}"">Calories: ${result.calories}</p>
+        <p id="protein${day}">Protein: ${result.protein}</p>
+      <img id="card-image${day}" class="card-image" src="${result.image}" alt="${result.title}">
 
+`}
 
+function watchRemoveClick() {
+   $('.js-output').on('click', '.js-remove-day', function(event) {
+  event.preventDefault();
+  let day = $(this).val()
+  let ingredient = ''
+  dietFilter = ''
+  allergyList = ['']
+  $(`#${day}Card`).html(renderNotCooking(day));
+  console.log(`${day} ${ingredient} option pressed`)
+  })
+}
+
+function renderNotCooking(day) {  
+  return `
+        <h3 id="recipe-title${day}" class="recipe-title">Not Cooking</h3>
+        <p id="calories${day}""> </p>
+        <p id="protein${day}"></p>
+      <img id="card-image${day}" class="card-image" src="https://www.displayfakefoods.com/store/pc/catalog/2189-lg.jpg" alt="Not cooking image">
+
+`}
+
+function watchStartOver() {
+  $('.js-menu-controls').on('click', '.js-start-over', function(event) {
+    event.preventDefault();
+    console.log("refresh clicked")
+    location.reload();
+  })
+}
+
+function watchRefreshMenuClick() {
+  $('.js-menu-controls').on('click', '.js-refresh-menu', function(event) {
+    dietFilter = ''
+    allergyList = ['']
+    dayIndex -= 7
+    getRecipesForWeek(allergyList, dietFilter);
+})
+}
 
 
 function handleMenuGenerator() {
@@ -268,6 +301,9 @@ function handleMenuGenerator() {
   watchSearchByIngredientClick();
   watchNextResultClick();
   watchPreviousResultClick();
+  watchRemoveClick();
+  watchRefreshMenuClick();
+  watchStartOver()
 }
 
 $(handleMenuGenerator)
